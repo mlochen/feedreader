4b7d858954d94c86dd9e4e35da3c80df55d5fb98
Marco Lochen <mlochen@protonmail.com>
2020-01-24 20:51:19 +0100

    improved style

--- feeds.py
+++ feeds.py
@@ -17,48 +17,55 @@
 
 from db_if import db_if
 import cgi
-import datetime
 import math
+import time
 
 def get_feed_list(active_feed_id):
-    s = ''
+    s = '<div id="feed_list"><form method="post">'\
+        '<table align="center" id="feed_table">'\
+        '<tr><th colspan="2">feeds</th></tr>'
     for feed in db.get_feeds(active_feed_id):
         tr_class = ' class="active_row"' if (feed['feed_id'] == active_feed_id) else ''
+        feed_id = feed['feed_id']
         classes = []
         if (feed['disabled'] != 0):
             classes.append("disabled")
         if (feed['updated_items'] != 0):
             classes.append("new_items")
+        title = feed['title']
+        updated_items = ""
+        if (feed['updated_items'] != 0):
+            updated_items = " (" + str(feed['updated_items']) + ")"
 
-        s = s + f'<tr' + tr_class + '><td>'\
-            f'<input name="del" value="{feed["feed_id"]}" type="checkbox"></td>'\
-            f'<td class="{" ".join(classes)}">'\
-            f'<a href="/cgi-bin/feeds.py?id={feed["feed_id"]}">{feed["title"]} ({feed["updated_items"]})</a>'\
-            f'</td></tr>\n'
+        s = s + f'<tr{tr_class}><td>'\
+                f'<input name="del" value="{feed_id}" type="checkbox"></td>'\
+                f'<td class="{" ".join(classes)}">'\
+                f'<a href="/cgi-bin/feeds.py?id={feed_id}">{title}{updated_items}</a>'\
+                f'</td></tr>\n'
+    s = s + '</table>'\
+            '<input type="submit" class="button" value="delete"></form>'\
+            '<form method="post"><input name="add" class="button" type="text">'\
+            '<input type="submit" class="button" value="add"></form>'\
+            '</div>'
     return s
 
 def get_item_list(feed_id):
-    s = ''
+    s = '<div id="item_list">'
     for item in db.get_feed_items(feed_id):
-        timestring = datetime.datetime.fromtimestamp(item["published"]).strftime("%A, %d. %B %Y %I:%M%p")
+        classes = "item unseen" if (item['seen'] == 0) else "item"
+        link = item['link']
+        author = item['author'] + " - " if (item['author'] != "") else ""
+        title = item['title']
+        timestring = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(item['published']))
+        summary = item['summary']
 
-        if (item["seen"] == 0):
-            classes = "item unseen"
-        else:
-            classes = "item"
         s = s + f'<div class="{classes}">'\
-            f'<a class="title" href="{item["link"]}">'
-        if (item["author"] != ""):
-            s = s + f'{item["author"]} - '
-        s = s + f'{item["title"]}</a><br>'\
-            f'<div class="date">{timestring}</div><br>'\
-            f'<div class="summary">{item["summary"]}</div>'
-
-        s = s + get_enclosure_list(item["item_id"])
-
-        s = s + f'</div>'\
-            f'<hr>\n\n\n'
-            # alles auf seen setzen
+                f'<a class="title" href="{link}">{author}{title}</a><br>'\
+                f'<div class="date">{timestring}</div><br>'\
+                f'<div class="summary">{summary}</div>'
+        s = s + get_enclosure_list(item['item_id'])
+        s = s + '</div><hr>\n'
+    s = s + '</div>'
     return s
 
 def get_enclosure_list(item_id):
@@ -67,10 +74,12 @@ def get_enclosure_list(item_id):
     if (len(enclosures) != 0):
         s = s + '<ul>'
         for enclosure in enclosures:
-            s = s + f'<li class="enclosure">'\
-                    f'<a href="{enclosure["href"]}">{enclosure["href"].split("/")[-1]}</a>'\
-                    f' ({get_size_string(enclosure["length"])} {enclosure["type"]})'\
-                    f'</li>'
+            href = enclosure['href']
+            length = enclosure['length']
+            type = enclosure['type']
+
+            s = s + f'<li class="enclosure"><a href="{href}">{href.split("/")[-1]}</a>'\
+                    f' ({get_size_string(length)} {type})</li>'
         s = s + '</ul>'
     return s
 
@@ -84,13 +93,12 @@ db_path = "/srv/http/feeds/feeds.db"
 db = db_if(db_path)
 attr = cgi.FieldStorage()
 
-print("Content-type: text/html")
-print("")
-print('<!DOCTYPE HTML><html lang="en"><head><title>My Feeds</title>')
-print('<link rel="stylesheet" type="text/css" href="/feeds/style.css">')
-print('<link rel="icon" type="image/png" href="/feeds/icon.png">')
-print('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>')
-print('<div id="feed_list">')
+print('''Content-type: text/html
+
+<!DOCTYPE HTML><html lang="en"><head><title>My Feeds</title>
+<link rel="stylesheet" type="text/css" href="/feeds/style.css">
+<link rel="icon" type="image/png" href="/feeds/icon.png">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>''')
 
 if ('add' in attr):
     db.add_feed(attr.getfirst("add"))
@@ -103,20 +111,10 @@ feed_id = -1
 if ('id' in attr):
     feed_id = int(attr['id'].value)
 
-print('<form method="post">')
-print('<table align="center" id="feed_table">')
-print('<tr><th colspan="2">feeds</th></tr>')
-
 print(get_feed_list(feed_id))
-
-print('</table>')
-print('<input type="submit" value="delete"></form>')
-print('<form method="post"><input name="add" type="text"><input type="submit" value="add"></form>')
-print('</div>')
-print('<div id="item_list">')
 
 if ('id' in attr):
     print(get_item_list(feed_id))
 
-print('</div>')
 print('</body></html>')
+
