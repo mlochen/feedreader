15c2ea42d705d590a974ab3ed2d76fd439d2b1e8
Marco Lochen <mlochen@protonmail.com>
2020-01-24 16:45:54 +0100

    added copyright headers

--- db_if.py
+++ db_if.py
@@ -1,5 +1,20 @@
 #!/usr/share/python3
 
+# Copyright (C) 2020 Marco Lochen
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sqlite3
 import time
 
@@ -67,4 +82,3 @@ class db_if:
             values = (None, item_id) + enclosure
             self.cursor.execute("INSERT INTO enclosures VALUES (?, ?, ?, ?, ?)", values)
         self.conn.commit()
-
--- feedsd.py
+++ feedsd.py
@@ -1,5 +1,20 @@
 #!/usr/share/python3
 
+# Copyright (C) 2020 Marco Lochen
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import calendar
 from db_if import db_if
 import feedparser
@@ -94,4 +109,3 @@ db = db_if(db_path)
 while (True):
     refresh_feeds(db)
     time.sleep(60)
-
